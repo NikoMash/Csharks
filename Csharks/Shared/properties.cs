13ab4f92d2791062279010b4da24b68071cb1fcc
Nikolaj Sievertsen Nøring <nikosn@hotmail.dk>
2022-03-29 12:38:18 +0200

    Class

---
+++ Csharks/Shared/properties.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharks.Shared
+{
+    internal class properties
+    //Defining members
+    {
+        public string cvr_navn, navn, beskrivelse, lang_beskrivelse, handicap, vejnavn, postnr_by;
+        public char kontakt_ved, husnr;
+        public int antal_pl, postnr;
+
+        //constructor 
+        public properties()
+        {
+
+        }
+
+        //constructor 
+        public properties(string cn, string n, string b, string lb, string h, string vn, string pnb, char kv, char hn, int ap, int pn)
+        {
+            cvr_navn = cn;
+            navn = n;
+            beskrivelse = b;
+            lang_beskrivelse = lb;
+            handicap = h;
+            vejnavn = vn;
+            postnr_by = pnb;
+            postnr = pn;
+            kontakt_ved = kv;
+            husnr = hn;
+            antal_pl = ap;
+            postnr = pn;
+        }
+        public string CVR_navn
+        {
+            get { return cvr_navn; }
+            set { cvr_navn = value; }
+        }
+        public string Navn
+        {
+            get { return navn; }
+            set { navn = value; }
+        }
+        public string Beskrivelse
+        {
+            get { return beskrivelse; }
+            set { beskrivelse = value; }
+        }
+        public string Lang_beskrivelse
+        {
+            get { return lang_beskrivelse; }
+            set { lang_beskrivelse = value; }
+        }
+        public string Handicap
+        {
+            get { return handicap; }
+            set { handicap = value; }
+        }
+        public string Vejnavn
+        {
+            get { return vejnavn; }
+            set { vejnavn = value; }
+        }
+        public string Postnr_by
+        {
+            get { return postnr_by; }
+            set { postnr_by = value; }
+        }
+        public char Kontakt_ved
+        {
+            get { return kontakt_ved; }
+            set { kontakt_ved = value;}
+        }
+        public char Husnr
+        {
+            get { return husnr; }
+            set { husnr = value; }
+        }
+        public int Antal_pl
+        {
+            get { return antal_pl; }
+            set { antal_pl = value; }
+        }
+        public int Postnr
+        {
+            get { return postnr; }
+            set { postnr = value; }
+        }
+    }
+
+}
---
+++ Csharks/Shared/shelter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csharks.Shared;
+
+namespace Csharks.Shared
+{
+    public class shelter
+    {
+        public ObjectId id { get; set; }
+        public properties? Properties { get; set; }
+    }
+}
